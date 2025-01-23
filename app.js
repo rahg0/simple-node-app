e097b6fbd785d249d5cd5e27be45223e469960dd
Raghu <raghu.esh@gmail.com>
2025-01-23 09:35:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var colors = require('colors');
+var toUpper = require('to-upper');
+console.log(colors.rainbow("Color changed to rainbow, via color npm package..."));
+console.log(toUpper("lowercase to uppercase via to uppercase npm package..."));
